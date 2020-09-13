4beb2509691baa17c709e5da01a9e66d61f21f51
Adrian Nicolas <liceurtwo@gmail.com>
2020-09-13 01:15:12 -0400

    Added typing

--- utils/database.py
+++ utils/database.py
@@ -1,24 +1,28 @@
-import sqlite3
+from typing import List, Dict, Union
+
 from .database_connection import DatabaseConnection
 
 """
 Concerned with storing and retrieving books from a database.
 """
 
+Book = Dict[str, Union[str, int]]
 
-def create_book_table():
+
+def create_book_table() -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
         cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)')
 
-def add_book(name, author):
+
+def add_book(name: str, author: str) -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
 
         cursor.execute(f'INSERT INTO books VALUES(?,?,0)', (name, author))
 
 
-def get_all_books():
+def get_all_books() -> List[Book]:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
 
@@ -28,14 +32,14 @@ def get_all_books():
         return books
 
 
-def mark_book_as_read(name):
+def mark_book_as_read(name: str) -> None:
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
 
         cursor.execute(f'UPDATE books SET read = 1 WHERE name=?',(name,))
 
 
-def delete_book(name):
+def delete_book(name: str):
     with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
 
